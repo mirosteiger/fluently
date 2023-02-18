439a0b3db7b967b86064646eb7382d5257635eb1
mirosteiger <52956540+mirosteiger@users.noreply.github.com>
2023-02-18 12:57:00 +0100

    firebase

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,10 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `fluently.`,
     siteUrl: `https://www.yourdomain.tld`,
-    description: "learning app to grind spanish"
+    description: "learning app to grind spanish",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -18,11 +18,24 @@ const config: GatsbyConfig = {
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
-      }
+      },
     },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyDsCQpVPIgl9LBp0eWs-ibAk1ZdKishSPo",
+          authDomain: "fluently-d1389.firebaseapp.com",
+          projectId: "fluently-d1389",
+          storageBucket: "fluently-d1389.appspot.com",
+          messagingSenderId: "399565633167",
+          appId: "1:399565633167:web:b4e042387e49eeecbee2fa",
+        },
+      },
+    },
   ],
-}
+};
 
-export default config
+export default config;
